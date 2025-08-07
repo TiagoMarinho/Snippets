efe006d866927f314ca91849d906ff8ea7ba68b2
TiagoMarinho <tiagomeraki@gmail.com>
2025-08-08 01:52:14 +0200

    remove unused code

--- src/contextmenu/snippet/data.mjs
+++
@@ -1,7 +0,0 @@
-import { ApplicationCommandType, ApplicationCommandOptionType } from "discord.js"
-
-export default {
-	name: `Copy as snippet`,
-	description: `Saves the message as a new snippet`,
-	type: ApplicationCommandType.Message,
-}
